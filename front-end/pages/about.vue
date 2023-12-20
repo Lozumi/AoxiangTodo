720bf19e0bee0e397515bd78ad6c417084bcbb14
Lozumi Everkine <fantasynico1026@gmail.com>
2023-12-20 20:46:52 +0800

    add: Update the structure

---
+++ front-end/pages/about.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section>
+    <p>此页面将显示在 /about 路由。</p>
+  </section>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ front-end/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>欢迎来到首页</h1>
+    <AppAlert>
+      这是一个自动导入的组件。
+    </AppAlert>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
