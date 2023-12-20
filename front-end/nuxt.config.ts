374f7dc8f3cd992a71c4bb9989137fc84d25540a
Lozumi Everkine <fantasynico1026@gmail.com>
2023-12-20 21:24:06 +0800

    add: Add Naive-UI

--- front-end/nuxt.config.ts
+++ front-end/nuxt.config.ts
@@ -1,4 +1,26 @@
+// import { defineNuxtConfig } from 'nuxt'
+//
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  build: {
+    transpile:
+        process.env.NODE_ENV === 'production'
+            ? [
+              'naive-ui',
+              'vueuc',
+              '@css-render/vue3-ssr',
+              '@juggle/resize-observer',
+                'date-fns'
+            ]
+            : ['@juggle/resize-observer']
+  },
+  vite: {
+    optimizeDeps: {
+      include:
+          process.env.NODE_ENV === 'development'
+              ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
+              : []
+    }
+  }
 })
