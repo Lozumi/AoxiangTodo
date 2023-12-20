3722ea2b0ed1d7f33fda6e6aabb488f503235475
Lozumi Everkine <fantasynico1026@gmail.com>
2023-12-20 21:44:28 +0800

    fix: Fix Naive-UI

--- front-end/nuxt.config.ts
+++ front-end/nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
               'vueuc',
               '@css-render/vue3-ssr',
               '@juggle/resize-observer',
-                'date-fns'
+                'date-fns-tz'
             ]
             : ['@juggle/resize-observer']
   },
@@ -19,7 +19,8 @@ export default defineNuxtConfig({
     optimizeDeps: {
       include:
           process.env.NODE_ENV === 'development'
-              ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
+              // ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
+                ? ['naive-ui', 'vueuc']
               : []
     }
   }
