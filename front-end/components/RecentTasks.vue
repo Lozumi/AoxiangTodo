f48b5c6efb5416a1593190785995c7d16f99e04a
刘黎可 <88770456@gitlab.com>
2023-12-21 21:29:30 +0800

    add: Add guide tab to main page

---
+++ front-end/components/RecentTasks.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<!-- RecentTasks.vue -->
+<template>
+  <div>
+    <h1>最近待办</h1>
+    <!-- 添加最近待办的内容 -->
+  </div>
+</template>
+
+<style scoped>
+
+</style>
