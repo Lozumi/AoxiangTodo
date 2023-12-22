bfd118987a12ce4535acf0d4c8f3d6dabe83625b
贾聪毅 <50274621@gitlab.com>
2023-12-22 12:49:08 +0800

    added jackson-core jars.

--- back-end/src/Main.java
+++ back-end/src/Main.java
@@ -1,6 +1,15 @@
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World! This is the initial project for back-end development!");
+    public static void main(String[] args) throws IOException {
     }
 }
