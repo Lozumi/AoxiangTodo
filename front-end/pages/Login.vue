612bca7cbc6ad56f6fcfba76bc0b4500b60d53ab
86201464 <86201464@gitlab.com>
2023-12-26 20:46:53 +0800

    hzq-login这哪一版啊

--- front-end/pages/Login.vue
+++ front-end/pages/Login.vue
@@ -22,20 +22,22 @@
             <v-window-item value="login">
 
               <div>
+
                 <div class="text-h6 mb-1">
                   LOGIN
                 </div>
                 <v-text-field
-
                     label="请输入账号"
-                    :rules="rules"
+                    :rules="usernameRules"
                     hide-details="auto"
+                    v-model="username"
                 ></v-text-field>
 
                 <v-text-field
                     label="请输入密码"
-                    :rules="rules"
+                    :rules="passwordRules"
                     hide-details="auto"
+                    v-model="password"
                 ></v-text-field>
 
                 <v-btn
@@ -44,7 +46,7 @@
                     height="48"
                     variant="tonal"
                     align-end
-                    @click="load"
+                    @click="login"
 
                 >
                   登入
@@ -58,34 +60,36 @@
               <div class="text-h6 mb-1">
                 REGISTER
               </div>
-              <div>
+
 
                 <v-text-field
-                    label="设置账号"
-                    :rules="rules"
+                    label="请设置账号"
+                    :rules="usernameRules"
                     hide-details="auto"
-                    class="custom-height custom-input"
+                    v-model="registerUsername"
                 ></v-text-field>
 
                 <v-text-field
                     label="设置用户名"
-                    :rules="rules"
+                    :rules="usernameRules"
                     hide-details="auto"
-                    class="custom-height custom-input"
+                    v-model="registerDisplayName"
                 ></v-text-field>
 
                 <v-text-field
                     label="设置密码"
-                    :rules="rules"
+                    :rules="passwordRules"
+                    type = "password"
                     hide-details="auto"
-                    class="custom-height custom-input"
+                    v-model="registerPassword"
                 ></v-text-field>
 
                 <v-text-field
                     label="确认密码"
-                    :rules="rules"
+                    :rules="confirmPasswordRules"
+                    type = "password"
                     hide-details="auto"
-                    class="custom-height custom-input"
+                    v-model="confirmRegisterPassword"
                 ></v-text-field>
                 <v-btn
                     :loading="loading"
@@ -93,11 +97,11 @@
                     height="48"
                     variant="tonal"
                     align-end
-                    @click="load"
+                    @click="register"
                 >
                   注册
                 </v-btn>
-              </div>
+
             </v-window-item>
 
           </v-window>
@@ -107,29 +111,95 @@
     </v-row>
   </v-card>
 
-
 </template>
 
+
+
 <script>
+
+import { required } from "vuelidate/lib/validators";
+
 export default {
   data: () => ({
+    users: JSON.parse(localStorage.getItem("users")) || [],
     loading: false,
+    tab: null, // 添加这行代码以初始化 tab 属性
+    username: "",
+    password: "",
+    registerUsername: "",
+    registerDisplayName: "",
+    registerPassword: "",
+    confirmRegisterPassword: "",
+    usernameRules: [required],
+    passwordRules: [required],
+    confirmPasswordRules: [
+      required,
+      (value) => value === this.registerPassword || "密码不一致",
+    ],
     rules: [
+      // 根据需要定义表单验证规则
       // value => !!value || 'Required.',
       // value => (value && value.length >= 3) || 'Min 3 characters',
       null
-
     ],
-    tab: null,
   }),
-  methods: {
-    load() {
-      this.loading = true
-      setTimeout(() => (this.loading = false), 3000)
+  computed: {
+    $v() {
+      return useVuelidate(this, {
+        username: this.usernameRules,
+        password: this.passwordRules,
+        registerUsername: this.usernameRules,
+        registerDisplayName: this.usernameRules,
+        registerPassword: this.passwordRules,
+        confirmRegisterPassword: this.confirmPasswordRules,
+      });
     },
   },
-}
+  methods: {
+    load() {
+      this.loading = true;
+      setTimeout(() => (this.loading = false), 3000);
+    },
+    login() {
+      let foundUser = this.users.find(
+          (user) => user.username === this.username && user.password === this.password
+      );
 
+      if (foundUser) {
+        this.$router.push({
+          name: "todo",
+          query: { username: this.username },
+        });
+      } else {
+        alert("账号或密码输入错误");
+        this.password = "";
+      }
+    },
+    register() {
+      this.$v.$touch(); // 添加这行代码以触发表单验证
+
+      if (!$v.$invalid) {
+        // 检查注册信息是否完整和正确
+        if (
+            !$v.registerUsername.$invalid &&
+            !$v.registerDisplayName.$invalid &&
+            !$v.registerPassword.$invalid &&
+            !$v.confirmRegisterPassword.$invalid &&
+            this.registerPassword === this.confirmRegisterPassword
+        ) {
+          this.tab = "login";
+        } else {
+          alert("请填写完整且正确的注册信息");
+        }
+      } else {
+        alert("请填写完整且正确的注册信息");
+      }
+    },
+    toRegister() {
+      this.$router.push("register");
+    },
+  },
+};
 </script>
 <style>
 /* 应用整体样式 */
@@ -188,6 +258,19 @@ body {
   background-color: white; /*输入框颜色*/
 }
 
+.v-input--is-danger {
+  margin-bottom: inherit !important;
+}
+
+/* 确保输入框之间的固定间距 */
+.v-text-field:not(:last-child) {
+  margin-bottom: 18px; /* 或者您想要的固定间距值 */
+}
+
+/* 可选：如果需要调整错误提示文本的样式 */
+.v-messages__message {
+  font-size: 14px;
+}
 .v-text-field__slot label {
   font-size: 14px;
   color: #1f1e33; /*这谁*/
