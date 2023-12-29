3c7de6c29428513ab9c71cde5ce78f3d20853fea
刘黎可 <88770456@gitlab.com>
2023-12-30 01:16:38 +0800

    fix: Fix response pack fetching and parsing.

--- front-end/pages/TodoPage/PostTest.vue
+++ front-end/pages/TodoPage/PostTest.vue
@@ -1,52 +1,89 @@
 <template>
-  <div>
-    <h1>Data Fetching Example</h1>
+  <v-container full-height>
+    <v-card class="mt-5 pa-5">
+      <h1 class="text-h2 mb-5">网络通信测试</h1>
 
-    <div v-if="result">
-      <h2>Status:</h2>
-      <p>Status: {{ result.status }}</p>
-      <p>Message: {{ result.message }}</p>
-      <p>Content: {{ result.content }}</p>
-    </div>
+      <v-btn @click="sendPostRequest" class="mb-5">Send POST Request</v-btn>
 
-    <div v-if="error">
-      <h2>Error:</h2>
-      <pre>{{ error }}</pre>
-    </div>
+      <v-card v-if="result" class="mt-5">
+        <v-card-title class="border-bottom mb-4">
+          <p class="text-h5">返回包：</p>
+        </v-card-title>
+        <v-card-text>
+          <p class="text-h5 mb-5">原文：{{ result }}</p>
 
-    <div v-if="pending">Loading...</div>
-  </div>
+          <!-- 使用 v-data-table 来展示表格 -->
+          <v-data-table
+              :headers="tableHeaders"
+              :items="[result]"
+              :items-per-page="5"
+              class="elevation-1"
+          ></v-data-table>
+        </v-card-text>
+      </v-card>
+
+      <v-alert v-if="error" type="error" class="mt-5">
+        <h2 class="text-h3">Error:</h2>
+        <pre>{{ error }}</pre>
+      </v-alert>
+
+      <v-progress-linear v-if="pending" class="mt-5"></v-progress-linear>
+    </v-card>
+  </v-container>
 </template>
 
 <script setup lang="ts">
 
-const apiUrl = 'http://localhost:20220/'; // Replace with your API endpoint
+const sendPostRequest = async () => {
+  await refresh();
+};
+
+const apiUrl = 'http://localhost:20220/todoworkitem';
+const requestType = 'CreateToDoWork';
+const Content = JSON.stringify({
+  '@class': 'shared.ToDoWorkItem',
+  layer : 1,
+  innerId : 0,
+  importancePriority : 1,
+  emergencyPriority : 1,
+  title : '测试事项',
+  subtitle : '测试子事项',
+  description : '测试描述',
+  createTime : '2023-12-29T20:20:20',
+  startTime : '2023-12-29T21:20:20',
+  deadLine : '2023-12-29T22:20:20',
+  status : 'Activated',
+  subToDoWorkItemInnerIdList: [],
+  pomodoroRecordInnerIdList : []
+});
+
 const { data, pending, error, refresh } = useFetch(apiUrl, {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
   },
-  body: JSON.stringify({ /* your request body */ }),
-  transformResponse: (text) => {
-    // Parse the JSON string returned from the API
-    try {
-      return JSON.parse(text);
-    } catch (error) {
-      console.error('Error parsing response:', error);
-      return null;
-    }
-  },
+  body: JSON.stringify({
+    content: Content,
+    requestType: requestType,
+  }),
 });
+
+// 使用 ref 来保存表格的头部信息
+const tableHeaders = ref([
+  { title: 'Status', value: 'status' },
+  { title: 'Message', value: 'message' },
+  { title: 'Content', value: 'content' },
+]);
 
 const result = ref(null);
 
-// Update the result when data changes
 onMounted(() => {
   refresh();
 });
 
-// Watch for changes in the fetched data
 watch(data, (newData) => {
-  result.value = newData;
+  // 将 JSON 字符串解析为对象
+  result.value = JSON.parse(newData);
 });
+
 </script>
