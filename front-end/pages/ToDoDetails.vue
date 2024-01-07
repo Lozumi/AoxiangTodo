9731e358b6c57ce9109f1b8f174f75eec5e2d5bf
傅钰 <27433118@gitlab.com>
2024-01-07 19:26:27 +0800

    待办详情界面2.0 完成待办任务的详细内容编辑，可以实现任务分类、任务命名、任务描述、任务时间选择、任务重要性和紧急程度选择、上传图片以及删除任务。

--- front-end/pages/ToDoDetails.vue
+++ front-end/pages/ToDoDetails.vue
@@ -4,7 +4,7 @@
       <!--应用栏-->
       <v-app-bar :elevation="0"
                  color="#3A8FB7">
-        <v-toolbar-title>待办清单</v-toolbar-title>
+        <v-toolbar-title>待办详情</v-toolbar-title>
         <template
             v-slot:append>
           <v-btn
@@ -17,45 +17,126 @@
           <v-list>
             <!--选择分类文件夹-->
             <v-list-item>
-              <v-list-icon>
-                <v-icon
-                    size = "36"
-                    color="#3A8FB7">
-                  mdi-folder-star-outline
-                </v-icon>
-              </v-list-icon>
-              <v-list-item-content
-                  style="font-weight: bold;
-                      font-size: 28px;
-                      color: #3A8FB7">
-                文件夹
-              </v-list-item-content>
-              <v-select
-                  clearable
-                  label="选择"
-                  :items="[]"
-                  variant="solo-filled"
-              ></v-select>
+              <v-bottom-sheet v-model="sheet">
+                <template v-slot:activator = "{ props }">
+                  <div>
+                    <v-btn v-bind = "props"
+                           elevation="0"
+                           :color="buttonColor">
+                      <v-icon size="24">mdi-folder-star-outline</v-icon>
+                      {{buttonText}}
+                    </v-btn>
+                  </div>
+                </template>
+
+                <v-list>
+                  <v-list-subheader>文件夹</v-list-subheader>
+                  <v-list-item
+                      v-for="(tile, index) in tiles"
+                      :key="tile.title"
+                      @click="changeSheet(tile.title);
+                          changeContent(tile.title);
+                          openDialog(tile.title);
+                          changeColor(tile.color)"
+                  >
+                    <template v-slot:prepend>
+                      <v-avatar :color="tile.color"
+                                size = "x-small"></v-avatar>
+                    </template>
+                    <v-list-content>
+                      <v-list-item-title>
+                        <!-- 使用条件语句判断是否要对该item实现特殊处理 -->
+                        <span v-if="tile.title === specialTitle"
+                              style="color: lightskyblue;">{{ tile.title }}</span>
+                        <span v-else>{{ tile.title }}</span>
+                      </v-list-item-title>
+                    </v-list-content>
+
+                    <template v-slot:append>
+                      <v-btn elevation="0">
+                        <v-icon>mdi-trash-can-outline</v-icon>
+                        <v-dialog
+                            v-model="_dialog"
+                            activator="parent"
+                            width="auto"
+                        >
+                          <v-card>
+                            <v-card-text>
+                              确定要删除这个文件夹吗？
+                            </v-card-text>
+                            <v-card-actions>
+                              <v-btn color="primary"
+                                     block
+                                     @click="_dialog = false">关闭</v-btn>
+                            </v-card-actions>
+                            <v-card-actions>
+                              <v-btn color="primary"
+                                     block
+                                     @click="_dialog = false;
+                                           removeFolder(index)">确定</v-btn>
+                            </v-card-actions>
+                          </v-card>
+                        </v-dialog>
+                      </v-btn>
+                    </template>
+
+                  </v-list-item>
+                </v-list>
+
+                <v-dialog
+                    v-model = "fileDialog"
+                    max-width="600">
+                  <v-card>
+                    <v-text-field v-model = "inputText"
+                                  label = "文件夹名称"
+                                  color="#3A8FB7"
+                    ></v-text-field>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="blue-darken-1"
+                             variant="text"
+                             @click="fileDialog = false"
+                      >
+                        关闭
+                      </v-btn>
+                      <v-btn color="blue-darken-1"
+                             variant="text"
+                             @click="fileDialog = false;
+                                 changeContent(inputText);
+                                 addFolder(inputText)"
+                      >
+                        保存
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-bottom-sheet>
             </v-list-item>
             <v-divider></v-divider>  <!--下划线-->
             <!--任务名-->
             <v-list-item>
               <v-text-field
                   label="任务名"
-                  variant="underlined"
-                  color="#3A8FB7"></v-text-field>
+                  color="#3A8FB7"
+              ></v-text-field>
               <!--任务描述-->
               <v-text-field
                   v-model="inputValue"
                   label="再来一点描述"
-                  variant="underlined"></v-text-field>
+                  variant="underlined"
+                  color="#536976"
+              ></v-text-field>
             </v-list-item>
 
             <!--任务时间-->
+            <!--开始日期-->
             <v-list-item>
+              <v-list-item-title style="font-weight: bold;
+                                        color: #9796f0">待办开始时间</v-list-item-title>
               <v-btn-toggle
                   v-model="toggle_exclusive"
-                  color="#3A8FB7">
+                  color="#9796f0"
+              >
                 <v-btn>
                   今天
                 </v-btn>
@@ -66,18 +147,57 @@
               </v-btn-toggle>
 
               <v-btn class="ma-2"
-                     color="#3A8FB7"
-                     @click="expand = !expand">
+                     @click="expand = !expand; togglePanel"
+                     style="color: #9796f0"
+                     elevation="0"
+              >
                 <v-icon>mdi-calendar</v-icon>
-                <v-btn-title>自定义</v-btn-title>
+                <v-btn-title>{{formattedDate}}</v-btn-title>
               </v-btn>
 
               <v-expand-transition>
                 <v-card
                     v-show="expand"
                 >
-                  <v-date-picker
-                      color="#3A8FB7"></v-date-picker>
+                  <v-date-picker color="#9796f0"
+                                 v-model="selectedDate"></v-date-picker>
+                </v-card>
+              </v-expand-transition>
+
+            </v-list-item>
+            <!--待办结束日期-->
+            <v-list-item>
+              <v-list-item-title style="font-weight: bold;
+                                        color: #ee9ca7">待办结束时间</v-list-item-title>
+              <v-btn-toggle
+                  v-model="toggle_exclusive_over"
+                  color="#ee9ca7"
+              >
+                <v-btn>
+                  今天
+                </v-btn>
+
+                <v-btn>
+                  明天
+                </v-btn>
+              </v-btn-toggle>
+
+              <v-btn class="ma-2"
+                     @click="expand_over = !expand_over;
+                         togglePanel_over"
+                     style="color: #ee9ca7"
+                     elevation="0"
+              >
+                <v-icon>mdi-calendar</v-icon>
+                <v-btn-title>{{formattedDate_over}}</v-btn-title>
+              </v-btn>
+
+              <v-expand-transition>
+                <v-card
+                    v-show="expand_over"
+                >
+                  <v-date-picker color="#ee9ca7"
+                                 v-model="selectedDateOver"></v-date-picker>
                 </v-card>
               </v-expand-transition>
 
@@ -88,7 +208,10 @@
               <v-card
                   class="mx-auto"
               >
-                <v-card-title style="font-weight: bold; color: #3A8FB7">任务重要程度</v-card-title>
+                <v-card-title style="font-weight: bold;
+                                         color: #3A8FB7">
+                  任务重要程度
+                </v-card-title>
                 <v-container>
                   <v-row
                       align="center"
@@ -103,7 +226,7 @@
                       <v-chip
                           :disabled="loading"
                           closable
-                          @click:close="selected.splice(i, 1)"
+                          @click:close="selected.splice(i, 1)"d
                       >
                         {{ selection.text }}
                       </v-chip>
@@ -159,10 +282,14 @@
                         确定要删除这个任务吗？
                       </v-card-text>
                       <v-card-actions>
-                        <v-btn color="primary" block @click="dialog = false">关闭</v-btn>
+                        <v-btn color="primary"
+                               block
+                               @click="dialog = false">关闭</v-btn>
                       </v-card-actions>
                       <v-card-actions>
-                        <v-btn color="primary" block @click="dialog = false">确定</v-btn>
+                        <v-btn color="primary"
+                               block
+                               @click="dialog = false">确定</v-btn>
                       </v-card-actions>
                     </v-card>
                   </v-dialog>
@@ -190,9 +317,31 @@ export default {
       toggle_none: null,
       toggle_one: 0,
       toggle_exclusive: 2,
+      toggle_exclusive_over: 2,
       toggle_multiple: [0, 1, 2],
       expand: false,
+      expand_over: false,
+      _dialog: false,
       dialog: false,
+      fileDialog: false,
+      buttonText: '未分类',
+      buttonColor: 'primary',
+      panelOpen: false,
+      sheet: false,
+      inputValue: '',
+      inputText: '',
+      selectedDate: null,
+      selectedDateOver: null,
+
+      tiles: [
+        { color: '#3A8FB7', title: '自定义'},
+        { color: '#dab8ee', title: '不选择文件夹' },
+        { color: '#96DEDA', title: '学习'},
+        { color: '#ffc500', title: '工作'},
+        { color: '#FFB88C', title: '日常'},
+        { color: '#49a09d', title: '旅行'},
+      ],
+      specialTitle: '自定义',
 
       items: [
         {
@@ -246,6 +395,27 @@ export default {
 
       return selections
     },
+
+    formattedDate () {
+      // 格式化日期，你可以根据需要选择合适的日期格式
+      if (this.selectedDate) {
+        const options = { year: 'numeric', month: 'short', day: 'numeric' };
+        return this.selectedDate.toLocaleDateString('en-US', options);
+      } else {
+        return '自定义';
+      }
+    },
+
+    formattedDate_over () {
+      // 格式化日期，你可以根据需要选择合适的日期格式
+      if (this.selectedDateOver) {
+        const options = { year: 'numeric', month: 'short', day: 'numeric' };
+        return this.selectedDateOver.toLocaleDateString('en-US', options);
+      } else {
+        return '自定义';
+      }
+    },
+
   },
 
   watch: {
@@ -270,6 +440,66 @@ export default {
       }, 2000)
     },
 
+    togglePanel() {
+      this.panelOpen = !this.panelOpen;
+    },
+
+    togglePanel_over() {
+      this.panelOpen = !this.panelOpen;
+    },
+
+    changeContent(parameter) {
+      if(parameter == '不选择文件夹'){
+        this.buttonText = '未分类';
+      }
+      else{
+        this.buttonText = parameter;
+      }
+    },
+
+    changeColor(parameter) {
+      if(parameter == '不选择文件夹'){
+        this.buttonColor = 'primary';
+      }
+      else{
+        this.buttonColor = parameter;
+      }
+    },
+
+    openDialog(parameter) {
+      if(parameter === '自定义'){
+        this.fileDialog = true;
+      }
+      else{
+        this.fileDialog = false;
+      }
+    },
+
+    changeSheet(parameter) {
+      if(parameter == '自定义'){
+        this.sheet = true;
+      }
+      else{
+        this.sheet = false;
+      }
+    },
+
+    addFolder(parameter) {
+      this.tiles.push({
+        color: this.getRandomColor(),
+        title: parameter,
+        index: this.tiles.length + 1,
+      });
+    },
+
+    getRandomColor() {
+      // 生成随机颜色的函数
+      return `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    },
+
+    removeFolder(parameter) {
+      this.tiles.splice(parameter,1);
+    }
   }
 }
 </script>
