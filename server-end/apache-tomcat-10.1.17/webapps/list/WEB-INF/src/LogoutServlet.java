fa821690c6e8ff7d08d0fa3b292c5d22594712d3
张书畅 <31248408@gitlab.com>
2024-01-08 09:48:24 +0000

    logout tested ok

---
+++ server-end/apache-tomcat-10.1.17/webapps/list/WEB-INF/src/LogoutServlet.java
@@ -0,0 +1,107 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class LogoutServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    private final String success = "Success";
+
+    private final String failure = "Failure";
+
+    private final int expectedParamCount = 1;
+
+    private Hashtable<String, String> parameters = new Hashtable<>() {{
+        put("token", "");
+    }};
+
+    private DBSingletonInstance dbSingletonInstance = DBSingletonInstance.getDbSingletonInstance();
+
+    public LogoutServlet() {
+        super();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws IOException, ServletException {
+        System.out.println("LogoutServlet::doGet");
+        System.out.println("Received: " + request.getRequestURL());
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+
+        Enumeration<String> parameterNames = request.getParameterNames();
+        int paramCount = 0;
+        while (parameterNames.hasMoreElements()) {
+            ++paramCount;
+            String paramRec = parameterNames.nextElement();
+            String valueRec = request.getParameter(paramRec);
+            if (parameters.containsKey(paramRec)) {
+                parameters.remove(paramRec);
+                parameters.put(paramRec, valueRec);
+                System.out.println("Parsed: %s - %s".formatted(paramRec, valueRec));
+            } else {
+                System.out.println("Error: wrong parameter name");
+                out.print(failure);
+                out.flush();
+                return;
+            }
+        }
+        if (paramCount != expectedParamCount) {
+            System.out.println("Error: wrong parameter count");
+            out.print(failure);
+            out.flush();
+            return;
+        }
+
+        Connection c = dbSingletonInstance.getConnectionToDB();
+        try {
+            Statement statement = c.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT * FROM tokens WHERE token=\"%s\";".formatted(parameters.get("token")));
+            if (!resultSet.next()) {
+                resultSet.close();
+                statement.close();
+                System.out.println("Error: user not found or not logged in");
+                out.print(failure);
+                out.flush();
+                return;
+            }
+
+            String token = resultSet.getString("token");
+            statement.executeUpdate("UPDATE users SET token=NULL where token=\"%s\";".formatted(token));
+            statement.executeUpdate(String.format("DELETE FROM tokens WHERE token=\"%s\"; ", token));
+            c.commit();
+            System.out.println("User logged in");
+            out.print(success);
+            out.flush();
+
+            resultSet.close();
+            statement.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            out.print(failure);
+            out.flush();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
