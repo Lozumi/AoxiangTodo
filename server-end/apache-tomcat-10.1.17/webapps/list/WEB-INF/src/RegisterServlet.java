6201f671ff702f2b9b80feb6b62c48d02464a32e
张书畅 <31248408@gitlab.com>
2024-01-08 03:37:28 +0000

    completed register-servlet

---
+++ server-end/apache-tomcat-10.1.17/webapps/list/WEB-INF/src/RegisterServlet.java
@@ -0,0 +1,108 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class RegisterServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    private final String success = "Success";
+
+    private final String failure = "Failure";
+
+    private final int expectedParamCount = 3;
+
+    private Hashtable<String, String> parameters = new Hashtable<>() {{
+        put("user-name", "");
+        put("account", "");
+        put("password", "");
+    }};
+
+    private DBSingletonInstance dbSingletonInstance = DBSingletonInstance.getDbSingletonInstance();
+
+    public RegisterServlet() {
+        super();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws IOException, ServletException {
+        System.out.println("RegisterServlet::doGet");
+        System.out.println("Received: " + request.getRequestURL());
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+
+        Enumeration<String> parameterNames = request.getParameterNames();
+        int paramCount = 0;
+        while (parameterNames.hasMoreElements()) {
+            ++paramCount;
+            String paramRec = parameterNames.nextElement();
+            String valueRec = request.getParameter(paramRec);
+            if (parameters.containsKey(paramRec)) {
+                parameters.remove(paramRec);
+                parameters.put(paramRec, valueRec);
+                System.out.println("Parsed: %s\t%s".formatted(paramRec, valueRec));
+            } else {
+                System.out.println("Error: wrong parameter name");
+                out.println(failure);
+                return;
+            }
+        }
+        if (paramCount != expectedParamCount) {
+            System.out.println("Error: wrong parameter count");
+            out.println(failure);
+            return;
+        }
+
+        Connection c = dbSingletonInstance.getConnectionToDB();
+        try {
+            Statement statement = c.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT * FROM users WHERE account=\"%s\";".formatted(parameters.get("account")));
+            if (resultSet.next()) {
+                resultSet.close();
+                statement.close();
+                System.out.println("Error: user already exists");
+                out.println(failure);
+                return;
+            }
+            System.out.println("here1");
+
+            statement.executeUpdate("""
+                    INSERT INTO users (account, user_name, password)
+                    VALUES ("%s", "%s", "%s");
+                    """.formatted(parameters.get("account"), parameters.get("user-name"), parameters.get("password")));
+            c.commit();
+            System.out.println("here2");
+            resultSet.close();
+            statement.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            out.println(failure);
+            return;
+        }
+
+        System.out.println("User registered successfully");
+        out.println(success);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
