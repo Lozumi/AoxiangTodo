70f337793f1c584541cc2f73244eaae1b5807bdd
刘黎可 <88770456@gitlab.com>
2024-01-09 16:55:43 +0800

    feat: Add AccountRequest test page

--- front-end/pages/TodoPage/PostTest_create.vue
+++ front-end/pages/TodoPage/PostTest_create.vue
@@ -35,31 +35,30 @@
 <script setup lang="ts">
 import ToDoWorkRequest from '~/composables/ToDoWorkRequest';
 import { ref, watch } from 'vue';
-import AccountRequest from "~/composables/AccountRequest";
 const result = ref(null);
 
 const sendPostRequest = async () => {
   await refresh();
 };
 
-  // const todoItem = JSON.stringify({
-  //   '@class': 'shared.ToDoWorkItem',
-  //   layer: 1,
-  //   innerId: 0,
-  //   importancePriority: 1,
-  //   emergencyPriority: 1,
-  //   title: '测试事项',
-  //   subtitle: '测试子事项',
-  //   description: '测试描述',
-  //   createTime: '2023-12-29T20:20:20',
-  //   startTime: '2023-12-29T21:20:20',
-  //   deadLine: '2023-12-29T22:20:20',
-  //   status: 'Activated',
-  //   subToDoWorkItemInnerIdList: [],
-  //   pomodoroRecordInnerIdList: [],
-  // });
+  const todoItem = JSON.stringify({
+    '@class': 'shared.ToDoWorkItem',
+    layer: 1,
+    innerId: 0,
+    importancePriority: 1,
+    emergencyPriority: 1,
+    title: '测试事项',
+    subtitle: '测试子事项',
+    description: '测试描述',
+    createTime: '2023-12-29T20:20:20',
+    startTime: '2023-12-29T21:20:20',
+    deadLine: '2023-12-29T22:20:20',
+    status: 'Activated',
+    subToDoWorkItemInnerIdList: [],
+    pomodoroRecordInnerIdList: [],
+  });
 
-  const {data, error, pending, refresh} = await AccountRequest.register('234','456','789');
+  const {data, error, pending, refresh} = await ToDoWorkRequest.create(todoItem);
 
   if (error) {
     console.error('Error:', error);
