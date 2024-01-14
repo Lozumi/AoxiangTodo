75e2c34f50ab8d40071ff2307de48248832e9ded
刘黎可 <88770456@gitlab.com>
2024-01-14 19:59:36 +0800

    feat: Add user logout method

--- front-end/pages/TodoPage.vue
+++ front-end/pages/TodoPage.vue
@@ -8,15 +8,20 @@
         <v-icon>mdi-palette</v-icon>
         主题
       </v-btn>
+      <v-btn v-if="isLogin" @click="userLogout" text>
+        <v-icon>mdi-logout</v-icon>
+        注销
+      </v-btn>
     </v-app-bar>
 
     <!-- 侧面导航栏 -->
     <v-navigation-drawer :width="200" app :color="selectedTheme.primaryColor" :dark="selectedTheme.darkSideNav">
       <v-list dense>
-        <v-list-item title="亲爱的{{}}}" subtitle="欢迎使用翱翔清单！"></v-list-item>
-<!--        <v-list-item class="nav-item" @click="$router.push('/TodoPage/TodayTasks')">-->
-<!--          <v-list-item-title class="white&#45;&#45;text">今日待办</v-list-item-title>-->
-<!--        </v-list-item>-->
+        <v-list-item v-if="isLogin" :title="`亲爱的 ${currentUser}`" subtitle="欢迎使用翱翔清单！"></v-list-item>
+        <v-list-item v-if="!isLogin" :title="`请登录`" subtitle="欢迎使用翱翔清单！"></v-list-item>
+        <!--        <v-list-item class="nav-item" @click="$router.push('/TodoPage/TodayTasks')">-->
+        <!--          <v-list-item-title class="white&#45;&#45;text">今日待办</v-list-item-title>-->
+        <!--        </v-list-item>-->
         <v-divider class="white--text"></v-divider>
         <v-list-item class="nav-item" @click="$router.push('/TodoPage/Todo')">
           <v-list-item-title class="white--text">最近待办</v-list-item-title>
@@ -58,6 +63,9 @@
 </template>
 
 <script>
+import AccountRequest from "~/composables/AccountRequest";
+import {ref} from 'vue';
+
 export default {
   data: () => ({
     themes: [
@@ -87,6 +95,8 @@ export default {
       },
     ],
     selectedThemeIndex: 0,
+    currentUser: '',
+    isLogin: false,
   }),
   computed: {
     selectedTheme() {
@@ -98,6 +108,46 @@ export default {
       // 切换到下一个主题
       this.selectedThemeIndex = (this.selectedThemeIndex + 1) % this.themes.length;
     },
+    userLogout() {
+      AccountRequest.userLogout()
+          .then(response => {
+            const { status, message, content} = JSON.parse(response.data.value);
+
+            if (status === "Success") {
+              this.isLogin = false;
+              this.$router.push('/TodoPage/Login'); // 跳转到登录页面
+            } else {
+              console.error("注销失败");
+            }
+          })
+          .catch(error => {
+            console.error(error);
+          });
+    },
+  },
+  created() {
+    AccountRequest.getCurrentUser()
+        .then(response => {
+          console.log(response);
+          const { status, message, content} = JSON.parse(response.data.value);
+          console.log(status);
+          console.log(content);
+          if (status === "Success") {
+
+            this.isLogin = true;
+            this.currentUser = content;
+            setTimeout(() => {
+              this.$router.push('/TodoPage/Todo');
+            }, 1000); // 等待1秒
+          } else {
+            setTimeout(() => {
+              this.$router.push('/TodoPage/Login');
+            }, 1000); // 等待1秒
+          }
+        })
+        .catch(error => {
+          console.error(error);
+        });
   },
 };
 </script>
